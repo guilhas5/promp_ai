52c33171bc6c4f5e53e326ff8f03bbcbfd2b04c4
Guilherme Costa <costaguilherme1995@gmail.com>
2023-08-07 18:48:36 +0100

    Implementing the auth with NextAuth and mongoDB

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [ToggleMenu, setToggleMenu] = useState(false);
+
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -32,7 +33,7 @@ const Nav = () => {
       </Link>
       {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -67,11 +68,12 @@ const Nav = () => {
       </div>
       {/*Mobile Navigation*/}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="assets/images/logo.svg"
+              src={session?.user.image}
               alt="profile image"
+              className="rounded-full"
               width={35}
               height={35}
               onClick={() => setToggleMenu((prev) => !prev)}
