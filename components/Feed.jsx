150998a03d6b731029775700f407de450ce1914b
Guilherme Costa <costaguilherme1995@gmail.com>
2023-08-08 22:51:18 +0100

    Implementing the PromptCart.Getting the Prompts from the DB to the Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PromptCart from "./PromptCart";
+const PromptCartList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCart
+        key={post.id}
+        post={post}
+        handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  )
+} 
+
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([])
+  
+  const handleSearchChange = (e) => {
+    e.preventDefault;
+    setSearchText(e.target.value);
+  };
 
-export default Feed
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch("/api/prompt")
+        if (response.ok) {
+          const data = await response.json()
+          setPosts(data)
+        }
+      } catch (error) {
+        console.error("Error fetching prompts",error)
+      }
+    }
+    fetchPosts()
+  },[])
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tar or a username"
+          value={searchText}
+          required
+          onChange={handleSearchChange}
+          className="search_input peer "
+        />
+      </form>
+      <PromptCartList data={posts}
+      handleTagClick={() => {}}/>
+    </section>
+  );
+};
+
+export default Feed;
--- components/PromptCart.jsx
+++ components/PromptCart.jsx
@@ -1,8 +1,54 @@
+"use client";
 
-const PromptCart = () => {
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+const PromptCart = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
   return (
-    <div>PromptCart</div>
-  )
-}
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={() => {}}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt="copy/tick icon"
+            width={15}
+            height={15}
+          />
+        </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
+};
 
-export default PromptCart
+export default PromptCart;
