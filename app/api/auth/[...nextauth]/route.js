e0433d8d0025d26c6b9879e6dc33e527adae1f67
Guilherme Costa <costaguilherme1995@gmail.com>
2023-08-09 15:52:23 +0100

    fixing sigin function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import { connectToDB } from "@utils/database";
 import User from "@models/user";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -13,38 +13,32 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-  
-      const sessionUser = await User.findOne({
-        email:session.user.email
-      })
-      session.user.id= sessionUser._id.toString()
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
-        const userExist = await User.findOne({
-          email: profile.email,
-        });
-  
-        if (!userExist) {
+
+        const userExists = await User.findOne({ email: profile.email });
+
+        if (!userExists) {
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
-            image:profile.picture
+            image: profile.picture,
           });
         }
-        return true
+
+        return true;
       } catch (error) {
-        console.log("Error checking if user exists",error.message)
-        return false
+        console.log("Error checking if user exists: ", error.message);
+        return false;
       }
     },
-
-  }
+  },
 });
+
 export { handler as GET, handler as POST };
-
-
-
-
